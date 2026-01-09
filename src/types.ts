045786c944cf887836a6bc88523852d40f24e0f3
jon-walker <jonathan.walker@ufl.edu>
2026-01-09 11:17:49 -0500

    feat: Define types used on Resources page

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+// Page: Resources 
+export interface Module {
+    id: string;
+    letter: string;
+    title: string;
+    description: string;
+    duration: string;
+    completed: boolean;
+    content: {
+      keyPoints: string[];
+      examples: string[];
+    };
+}
+
+export interface Video {
+    id: number;
+    title: string;
+    description: string;
+    url: string | null; // Optional
+    duration: string;
+    skills: string[];
+    host: "Youtube" | "Vimeo" | "Other";
+    embedCode: string | null; //Optional
+    // Possibly add thumbnail in the future
+}
+
+export interface ResourceListing {
+    id: number;
+    title: string;
+    description?: string; // Optional no default
+    url: string | null; // Optional
+    type: "link" | "document";
+    category: string;
+}
