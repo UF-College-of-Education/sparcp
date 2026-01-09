d24adc4dbc06beac52141a17f4a66ff75e369fe5
jon-walker <jonathan.walker@ufl.edu>
2026-01-09 14:52:37 -0500

    feat: Create placeholder training page

---
+++ src/pages/Training.tsx
@@ -0,0 +1,10 @@
+export function Training(){
+    return (
+        <div className="bg-gray-200 w-full h-screen border-">
+        
+            <p className="text-3xl text-center p-24">
+                The training session will go in an iframe here that links to the Unity App.
+            </p>
+        </div>
+    );
+}
