58bb7dedb515305743b10ca815b19f4e6c8e75a1
jon-walker <jonathan.walker@ufl.edu>
2026-03-19 14:44:37 -0400

    feat: Enable mdx for content editing

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import mdx from '@mdx-js/rollup'
 import path from 'node:path'
 
 // https://vitejs.dev/config/
-// BasicSsl will only load on vite dev 
+// BasicSsl will only load on vite dev
 export default defineConfig(({ command }) => ({
-  plugins: [react(), ...(command === 'serve' ? [basicSsl()] : [])],
+  plugins: [mdx(), react(), ...(command === 'serve' ? [basicSsl()] : [])],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
