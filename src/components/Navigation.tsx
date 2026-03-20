3c855a0b19c8df911c929745f2def659c27a27fe
jon-walker <jonathan.walker@ufl.edu>
2026-03-20 14:40:22 -0400

    feat: Hook up menu open/close buttons

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Card } from "./ui/card";
 import { Badge } from "./ui/badge";
 import { 
+  MenuIcon,
   MessageCircle, 
   BookOpen, 
   BarChart3, 
@@ -14,6 +15,8 @@ import {
 } from "lucide-react";
 import { NavLink } from "react-router";
 import { useAuth } from "../context/AuthContext";
+import { MenuTrayControlButton } from "./MenuTrayControlButton";
+import { ClearGuide } from "./clear-guide/ClearGuide";
 
 const navigationItems = [
   { id: "home", label: "Home", icon: House, route: "/", submenu: false },
@@ -28,11 +31,15 @@ export function Navigation() {
   const {logout} = useAuth();
 
   return (
-    <div className={`bg-card border-r transition-all duration-300 md:h-screen ${isCollapsed ? "w-16" : "w-64"}`}>
-      <div className="p-4 border-b">
-        <div className="flex items-center gap-3">
-          <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center">
-            <MessageCircle className="w-4 h-4 text-primary-foreground" />
+    <div className={`bg-card relative border-r transition-all duration-300 md:h-screen ${isCollapsed ? "w-16 " : "w-64"}`}>
+      <div className={`p-2 border-b flex items-center justify-center transition-all duration-300 ${isCollapsed ? "w-16" : "w-64"}`}>
+        <div className={`flex items-center gap-0 ${isCollapsed ? 'h-auto' : 'h-20'}`}>
+          
+          <div 
+            className={`bg-primary rounded-lg flex items-center justify-center transition-all duration-300 delay-200 h-10 ${isCollapsed ? 'w-10 opacity-100' : 'w-0 opacity-0' }`} 
+            onClick={() => setIsCollapsed(prev => !prev)}
+          >
+            <MenuIcon className={`text-primary-foreground transition-all duration-300 h-6 delay-200 ${isCollapsed ? 'w-10 opacity-100' : 'w-0 opacity-0' }`} strokeWidth={3} />
           </div>
           {!isCollapsed && (
             <div>
@@ -44,53 +51,43 @@ export function Navigation() {
       </div>
 
       <div className="p-4">
-        {!isCollapsed && (
-          <Card className="p-3 mb-4 bg-primary/5 border-primary/20">
-            <div className="flex items-center gap-2 mb-2">
-              <User className="w-4 h-4 text-primary" />
-              <span className="font-medium text-sm">Dr. Sarah Williams</span>
-            </div>
-            <div className="flex items-center gap-2">
-              <Award className="w-3 h-3 text-primary" />
-              <span className="text-xs text-muted-foreground">Pediatrician</span>
-              <Badge variant="secondary" className="text-xs ml-auto">Pro</Badge>
-            </div>
-          </Card>
-        )}
-
-        <nav className="space-y-1">
-          <ul>
-          {navigationItems.map((item) => {
-            const Icon = item.icon;
-            
-            // Associate a submenu as a prop of the menu item
-            // Set click listener to fetch and render submenu when needed
-
-            return (                
-              <li className="text-sm" key={item.id} >
-                <NavLink to={item.route} className="flex items-center p-3 hover:bg-gray-100 rounded-md">
-                  <Icon className="w-4 h-4" /> 
-                  <span className="pl-5">{item.label}</span>
-                  <ChevronRight className="w-4 h-4 ml-auto" />
-                </NavLink>
-              </li>
+          <nav id="sparc-nav" className={`space-y-1 overflow-hidden transition-all duration-300 ${isCollapsed ? 'h-0 opacity-0' : 'delay-200 opacity-100 h-auto'}`} >
+            <ul>
+            {navigationItems.map((item) => {
+              const Icon = item.icon;
               
-            );
-          })}
+              // Associate a submenu as a prop of the menu item
+              // Set click listener to fetch and render submenu when needed
 
-            <li>
-              <button
-                className="text-sm mt-6 flex items-center p-3 hover:bg-gray-100 rounded-md"
-                onClick={logout}
-              >
-                <LogOut className="w-4 h-4" />
-                <span className="pl-5">Logout</span>
-              </button>
-            </li>
-          </ul>
-        </nav>
+              return (                
+                <li className="text-sm" key={item.id} >
+                  <NavLink to={item.route} className="flex items-center p-3 hover:bg-gray-100 rounded-md">
+                    <Icon className="w-4 h-4" /> 
+                    <span className="pl-5">{item.label}</span>
+                    <ChevronRight className="w-4 h-4 ml-auto" />
+                  </NavLink>
+                </li>
                 
+              );
+            })}
+
+              <li>
+                <button
+                  className="text-sm mt-6 flex items-center p-3 hover:bg-gray-100 rounded-md"
+                  onClick={logout}
+                >
+                  <LogOut className="w-4 h-4" />
+                  <span className="pl-5">Logout</span>
+                </button>
+              </li>
+            </ul>
+          </nav>
+        <MenuTrayControlButton
+          handleClick={() => setIsCollapsed(prev => !prev)}
+          isCollapsed={isCollapsed}
+        />
       </div>
+
     </div>
   );
 }
