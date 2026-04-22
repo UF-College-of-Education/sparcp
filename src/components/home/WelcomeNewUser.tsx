52f459f4f1e1cdde777e024f3f91df545492c1e9
jon-walker <jonathan.walker@ufl.edu>
2026-04-22 12:09:57 -0400

    feat: Update home page layout

--- src/components/home/WelcomeNewUser.tsx
+++ src/components/home/WelcomeNewUser.tsx
@@ -7,7 +7,7 @@ import { processVimeoEmbedCode } from '../../lib/utils';
 const videoProps = processVimeoEmbedCode( videoEmbedCode );
 
 const mdxComponents = {
-    h1: ({children}: ComponentPropsWithoutRef<"h1">)=>(<h1 className="font-bold mb-6 text-4xl">{children}</h1>),
+    h1: ({children}: ComponentPropsWithoutRef<"h1">)=>(<h1 className="font-bold mb-6 text-4xl text-center">{children}</h1>),
     h2: ({children}: ComponentPropsWithoutRef<"h2"> )=>(<h2 className="font-semibold text-2xl mb-2">{children}</h2>),
     h3: ({children}: ComponentPropsWithoutRef<"h3"> )=>(<h3 className="font-semibold mb-2">{children}</h3>),
     h4: ({children}: ComponentPropsWithoutRef<"h4"> )=>(<h4 className="font-semibold mb-2">{children}</h4>),
@@ -17,24 +17,26 @@ const mdxComponents = {
 export function WelcomeNewUser () {
 
     return (
-        <section className="grid grid-cols-1 md:grid-cols-2 gap-20 px-12 py-20 pr-20 mt-6 items-center">
-            <div className="sparc-column">
+        <section className="px-12 py-20 pr-20 mt-6">
+            <div className="sparc-column w-full">
                 <Content components={mdxComponents}/>
-                <Button className="bg-black">
-                    <Link to={ctaButtonLink}>{ctaButtonText}</Link>
-                </Button>
-            </div>
-
-            <div className="sparc-column2 aspect-video px-6">                
                 <iframe 
-                    src={`https://player.vimeo.com/video/${videoProps['videoId']}?h=${videoProps['hash']}&badge=0&autopause=0&player_id=0&app_id=58479`}
-                    width="600" height="338" 
+                    src={`https://player.vimeo.com/video/${videoProps['videoId']}?h=${videoProps['hash']}&badge=0&autopause=0&player_id=0&app_id=58479&autoplay=1&title=0`}
+                    width="800" height="450" 
                     allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" 
                     referrerPolicy="strict-origin-when-cross-origin" 
                     title={videoProps['title'] ?? ''} 
-                    className='border border-off-white'>
+                    className='border border-off-white mx-auto my-12'>
                 </iframe>
+                
+                <div className='text-center'>
+                    <Button className="bg-yellow text-black border-2 border-yellow font-bold text-md hover:bg-white">
+                        <Link to={ctaButtonLink}>{ctaButtonText}</Link>
+                    </Button>
+                </div>
             </div>
+
+                
         </section>
     );
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,62 +1,10 @@
-import { useState } from "react";
-import Placeholder1 from "../assets/placeholder-1.png";
-import Placeholder2 from "../assets/placeholder-2.png";
-import Placeholder3 from "../assets/placeholder-3.png";
 import { WelcomeNewUser } from "../components/home/WelcomeNewUser";
 
 export function Home() {
 
     return (
-        <main className="">
+        <main className="w-full">
             <WelcomeNewUser />
-
-            <section className="bg-gray-100 space-y-6 p-12 pr-20">
-                <div>
-                    <h2 className="text-2xl mb-4 font-semibold">What to expect for this training?</h2>
-                    <p className="max-w-[800px]">During this training, you will interact with an AI avatar using your microphone to have a realistic conversation with a mock patient. This training uses an AI model trained on past conversations from live training sessions, and it will respond to you as a parent might in the real world. At various checkpoints, you will also hear from a virtual coach who will provide feedback based on your conversation.</p>
-                </div>
-                
-                <div className="columns-3 gap-20 pt-8 pb-8">
-                    <figure className="flex flex-col items-center">
-                        <img 
-                            src={Placeholder1}
-                            alt="Doctor receives instructions"
-                            className="mb-4"
-                            width={"280"}
-                        />
-                        <figcaption className="">
-                            <h3 className="text-xl font-semibold mb-2">Instructions</h3>
-                            <p>You will receive specific instructions before each training segment.</p>
-                        </figcaption>
-                    </figure>
-
-                    <figure className="flex flex-col items-center">
-                        <img 
-                            src={Placeholder2}
-                            alt="Doctor interacts with online content"
-                            className="mb-4 text-center"
-                            width={"280"}
-                        ></img>
-                        <figcaption className="">
-                            <h3 className="text-xl font-semibold mb-2">Interact</h3>
-                            <p>Next you will speak with the virtual parent as if they are in the room with you.</p>
-                        </figcaption>
-                    </figure>
-
-                    <figure className="flex flex-col items-center">
-                        <img 
-                            src={Placeholder3}
-                            alt="Doctor receives feedback"
-                            className="mb-4 text-center"
-                            width={"280"}
-                        ></img>
-                        <figcaption className="">
-                            <h3 className="text-xl font-semibold mb-2">Feedback</h3>
-                            <p>A virtual coach will appear on screen to provide feedback on what went well during the session and what can be improved.</p>
-                        </figcaption>
-                    </figure>
-                </div>
-            </section>
         </main>
     );
 }
