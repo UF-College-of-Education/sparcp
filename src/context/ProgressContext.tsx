7946180cfb3915cbd38bcbb77829aac373480702
jon-walker <jonathan.walker@ufl.edu>
2026-05-05 14:44:29 -0400

    feat: Show post-training survey link

--- src/context/ProgressContext.tsx
+++ src/context/ProgressContext.tsx
@@ -5,6 +5,7 @@ type ProgressContext = {
     didacticComplete: boolean;
     setDidacticComplete: (v:boolean)=>void;
     unityActive: boolean;
+    unityComplete: boolean;
     surveyComplete: boolean;
     setSurveyComplete: (v:boolean)=>void;
 };
@@ -18,20 +19,31 @@ export function ProgressProvider ({ children }: { children: React.ReactNode }) {
 
     const [didacticComplete, setDidacticComplete] = useState(false);
     const [unityActive, setUnityActive] = useState(false);
+    const [unityComplete, setUnityComplete] = useState(false);
     const [surveyComplete, setSurveyComplete]  = useState(false);
     
     // Update unityActive based on the current url path
     const location = useLocation();
 
-    useEffect(()=>{    
+    useEffect(()=>{
         setUnityActive(['/training'].includes(location.pathname))
     }, [location]);
+
+    useEffect(()=>{
+        if (!unityActive) return;
+        const timer = setTimeout(() => {
+            window.localStorage.setItem('unityComplete', '1');
+            setUnityComplete(true);
+        }, 60000);
+        return () => clearTimeout(timer);
+    }, [unityActive]);
 
 
     return <ProgressContext.Provider 
         value={{
             didacticComplete,
             unityActive,
+            unityComplete,
             surveyComplete,
             setSurveyComplete,
             setDidacticComplete
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,19 @@
+import { PostTraining } from "../components/home/PostTraining";
 import { WelcomeNewUser } from "../components/home/WelcomeNewUser";
+import { useProgress } from "../context/ProgressContext";
 
 export function Home() {
 
+    const progress = useProgress();
+    const trainingComplete = progress.unityComplete;
+
     return (
         <main className="w-full">
-            <WelcomeNewUser />
+            {trainingComplete ?
+                <PostTraining /> :
+                <WelcomeNewUser />
+            }
+            
         </main>
     );
 }
